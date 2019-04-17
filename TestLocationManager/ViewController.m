dae7c320c00013f8e0c153f037c77472c9182254
APP技術部-陳冠志 <bruce@mitake.com.tw>
2019-04-18 01:03:30 +0800

    測試取得使用者座標

--- TestLocationManager/ViewController.m
+++ TestLocationManager/ViewController.m
@@ -3,12 +3,14 @@
 //  TestLocationManager
 //
 //  Created by Bruce Chen on 2019/4/17.
-//  Copyright © 2019 APP技術部-陳冠志. All rights reserved.
+//  Copyright © 2019 bruce. All rights reserved.
 //
 
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *latLabel;
+@property (weak, nonatomic) IBOutlet UILabel *longLabel;
 
 @end
 
@@ -16,8 +18,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    if ([CLLocationManager locationServicesEnabled]) {
+        self.lcManager = [[CLLocationManager alloc] init];
+        self.lcManager.delegate = self;
+        self.lcManager.distanceFilter = 100;
+        self.lcManager.desiredAccuracy = kCLLocationAccuracyBest;
+        [self.lcManager requestWhenInUseAuthorization];
+        [self.lcManager startUpdatingLocation];
+    }
 }
 
+- (IBAction)checkPermission:(id)sender {
+    
+    
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations{
+    
+    CLLocation *currentLocation = [locations lastObject];
+    if(currentLocation != nil){
+        self.latLabel.text = [NSString stringWithFormat:@"%.2f",currentLocation.coordinate.latitude];
+        self.longLabel.text = [NSString stringWithFormat:@"%.2f",currentLocation.coordinate.longitude];
+    }
+}
 
 @end
